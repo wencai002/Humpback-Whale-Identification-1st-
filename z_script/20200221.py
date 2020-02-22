814aedaa6093b3ca3d225ba389c8c427f098b7b6
wencai002 <cai.wenjie002@gmail.com>
2020-02-22 01:18:56 +0100

    20200221 Add Tutorial

--- z_script/20200221.py
+++ z_script/20200221.py
@@ -3,7 +3,140 @@ from tensorflow.keras.applications.mobilenet import preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 import tensorflow as tf
-tf.image.decode_jpeg()
+import sys
+old_stderr = sys.stderr
+sys.stderr = open('/dev/null', 'w')
+import keras
+sys.stderr = old_stderr
 
-model = MobileNet(input_shape=(224,224,3))
-embeded_train = model.predict(train_ds)
+### train the model of crapping
+
+from PIL import Image as pil_image
+from PIL.ImageDraw import Draw
+from os.path import isfile
+
+# def expand_path(p):
+#     if isfile('../input/whale-categorization-playground/train/' + p): return '../input/whale-categorization-playground/train/' + p
+#     if isfile('../input/whale-categorization-playground/test/' + p): return '../input/whale-categorization-playground/test/' + p
+#     return p
+
+def read_raw_image(p):
+    return pil_image.open(p)
+#
+# def draw_dot(draw, x, y):
+#     draw.ellipse(((x-5,y-5),(x+5,y+5)), fill='red', outline='red')
+#
+# def draw_dots(draw, coordinates):
+#     for x,y in coordinates: draw_dot(draw, x, y)
+#
+# def bounding_rectangle(list):
+#     x0, y0 = list[0]
+#     x1, y1 = x0, y0
+#     for x,y in list[1:]:
+#         x0 = min(x0, x)
+#         y0 = min(y0, y)
+#         x1 = max(x1, x)
+#         y1 = max(y1, y)
+#     return x0,y0,x1,y1
+#
+# filename,coordinates = data[0]
+# box = bounding_rectangle(coordinates)
+# img = read_raw_image(filename)
+# draw = Draw(img)
+# draw_dots(draw, coordinates)
+# draw.rectangle(box, outline='red')
+# img
+
+######################
+### train the model
+######################
+img_shape  = (128,128,1)
+anisotropy = 2.15
+
+import random
+import numpy as np
+from scipy.ndimage import affine_transform
+from keras.preprocessing.image import img_to_array
+
+# Read an image as black&white numpy array
+def read_array(p):
+    img = read_raw_image(p).convert('L')
+    return img_to_array(img)
+
+def build_transform(rotation, shear, height_zoom, width_zoom, height_shift, width_shift):
+    rotation        = np.deg2rad(rotation)
+    shear           = np.deg2rad(shear)
+    rotation_matrix = np.array([[np.cos(rotation), np.sin(rotation), 0], [-np.sin(rotation), np.cos(rotation), 0], [0, 0, 1]])
+    shift_matrix    = np.array([[1, 0, height_shift], [0, 1, width_shift], [0, 0, 1]])
+    shear_matrix    = np.array([[1, np.sin(shear), 0], [0, np.cos(shear), 0], [0, 0, 1]])
+    zoom_matrix     = np.array([[1.0/height_zoom, 0, 0], [0, 1.0/width_zoom, 0], [0, 0, 1]])
+    shift_matrix    = np.array([[1, 0, -height_shift], [0, 1, -width_shift], [0, 0, 1]])
+    return np.dot(np.dot(rotation_matrix, shear_matrix), np.dot(zoom_matrix, shift_matrix))
+
+# Compute the coordinate transformation required to center the pictures, padding as required.
+def center_transform(affine, input_shape):
+    hi, wi = float(input_shape[0]), float(input_shape[1])
+    ho, wo = float(img_shape[0]), float(img_shape[1])
+    top, left, bottom, right = 0, 0, hi, wi
+    if wi/hi/anisotropy < wo/ho: # input image too narrow, extend width
+        w     = hi*wo/ho*anisotropy
+        left  = (wi-w)/2
+        right = left + w
+    else: # input image too wide, extend height
+        h      = wi*ho/wo/anisotropy
+        top    = (hi-h)/2
+        bottom = top + h
+    center_matrix   = np.array([[1, 0, -ho/2], [0, 1, -wo/2], [0, 0, 1]])
+    scale_matrix    = np.array([[(bottom - top)/ho, 0, 0], [0, (right - left)/wo, 0], [0, 0, 1]])
+    decenter_matrix = np.array([[1, 0, hi/2], [0, 1, wi/2], [0, 0, 1]])
+    return np.dot(np.dot(decenter_matrix, scale_matrix), np.dot(affine, center_matrix))
+
+# Apply an affine transformation to an image represented as a numpy array.
+def transform_img(x, affine):
+    matrix   = affine[:2,:2]
+    offset   = affine[:2,2]
+    x        = np.moveaxis(x, -1, 0)
+    channels = [affine_transform(channel, matrix, offset, output_shape=img_shape[:-1], order=1,
+                                 mode='constant', cval=np.average(channel)) for channel in x]
+    return np.moveaxis(np.stack(channels, axis=0), 0, -1)
+
+# Read an image for validation, i.e. without data augmentation.
+def read_for_validation(p):
+    x  = read_array(p)
+    t  = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    t  = center_transform(t, x.shape)
+    x  = transform_img(x, t)
+    x -= np.mean(x, keepdims=True)
+    x /= np.std(x, keepdims=True) + K.epsilon()
+    return x,t
+
+# Read an image for training, i.e. including a random affine transformation
+from keras import backend as K
+def read_for_training(p):
+    x  = read_array(p)
+    t  = build_transform(
+            random.uniform(-5, 5),
+            random.uniform(-5, 5),
+            random.uniform(0.9, 1.0),
+            random.uniform(0.9, 1.0),
+            random.uniform(-0.05*img_shape[0], 0.05*img_shape[0]),
+            random.uniform(-0.05*img_shape[1], 0.05*img_shape[1]))
+    t  = center_transform(t, x.shape)
+    x  = transform_img(x, t)
+    x -= np.mean(x, keepdims=True)
+    x /= np.std(x, keepdims=True) + K.epsilon()
+    return x,t
+
+# Transform corrdinates according to the provided affine transformation
+def coord_transform(list, trans):
+    result = []
+    for x,y in list:
+        y,x,_ = trans.dot([y,x,1]).astype(np.int)
+        result.append((x,y))
+    return result
+
+
+# tf.image.decode_jpeg()
+#
+# model = MobileNet(input_shape=(224,224,3))
+# embeded_train = model.predict(train_ds)
