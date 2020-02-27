18153ebbd92997fa8799c7de36f7939bfeff1efd
wencai002 <cai.wenjie002@gmail.com>
2020-02-27 19:50:13 +0100

    20200227 Modell is not good at all

--- z_script/Submission_20200226.py
+++ z_script/Submission_20200226.py
@@ -1,3 +1,6 @@
+#########################################
+### import the input image data
+#########################################
 import pickle
 with open('/home/wencai/PycharmProjects/WhaleIP/Humpback-Whale-Identification-1st-/z_script/cropped_img_test.pickle', 'rb') as f:
     dict_cropped_img_test = pickle.load(f)
@@ -20,20 +23,68 @@ with open('/home/wencai/PycharmProjects/WhaleIP/Humpback-Whale-Identification-1s
 print(len(dict_cropped_img_train3))
 
 from keras.models import load_model
-standard_model = load_model("/home/wencai/PycharmProjects/WhaleIP/mpiotte-standard.model")
-img_a = dict_cropped_img_test["PM-WWA-20170321-046.jpg"]
+#standard_model = load_model("/home/wencai/PycharmProjects/WhaleIP/mpiotte-standard.model")
+bootstrap_model = load_model("/home/wencai/PycharmProjects/WhaleIP/mpiotte-bootstrap.model")
+print(bootstrap_model.summary())
+#img_a = dict_cropped_img_test["PM-WWA-20170321-046.jpg"]
 
+################################################################
+### build the DataFrame
+################################################################
 import pandas as pd
-score_img_a = pd.DataFrame()
-for i in range(len(dict_cropped_img_test)):
-    p_name = list(dict_cropped_img_test.keys())[i]
-    score_img_a.loc[i,"p_name"] = p_name
-    img_b = dict_cropped_img_test[p_name]
-    score = standard_model.predict([img_a,img_b])[0][0]
-    score_img_a.loc[i,"score"] = score
+for j in range(len(dict_cropped_img_test)):
+    img_a_name = list(dict_cropped_img_test.keys())[j]
+    img_a = dict_cropped_img_test[img_a_name]
 
-score_img_a.to_csv("/home/wencai/PycharmProjects/WhaleIP/img_a_score.csv")
-print(score_img_a.head(30))
+    score_test_img_a = pd.DataFrame()
+    for i in range(len(dict_cropped_img_test)):
+        p_name = list(dict_cropped_img_test.keys())[i]
+        score_test_img_a.loc[i,"p_name"] = p_name
+        img_b = dict_cropped_img_test[p_name]
+        score = bootstrap_model.predict([img_a,img_b])[0][0]
+        score_test_img_a.loc[i,"score"] = score
+
+    score_train0_img_a = pd.DataFrame()
+    for i in range(len(dict_cropped_img_train0)):
+        p_name = list(dict_cropped_img_train0.keys())[i]
+        score_train0_img_a.loc[i,"p_name"] = p_name
+        img_b = dict_cropped_img_train0[p_name]
+        score = bootstrap_model.predict([img_a,img_b])[0][0]
+        score_train0_img_a.loc[i,"score"] = score
+
+    score_train1_img_a = pd.DataFrame()
+    for i in range(len(dict_cropped_img_train1)):
+        p_name = list(dict_cropped_img_train1.keys())[i]
+        score_train1_img_a.loc[i,"p_name"] = p_name
+        img_b = dict_cropped_img_train1[p_name]
+        score = bootstrap_model.predict([img_a,img_b])[0][0]
+        score_train1_img_a.loc[i,"score"] = score
+
+    score_train2_img_a = pd.DataFrame()
+    for i in range(len(dict_cropped_img_train2)):
+        p_name = list(dict_cropped_img_train2.keys())[i]
+        score_train2_img_a.loc[i,"p_name"] = p_name
+        img_b = dict_cropped_img_train2[p_name]
+        score = bootstrap_model.predict([img_a,img_b])[0][0]
+        score_train2_img_a.loc[i,"score"] = score
+
+    score_train3_img_a = pd.DataFrame()
+    for i in range(len(dict_cropped_img_train3)):
+        p_name = list(dict_cropped_img_train3.keys())[i]
+        score_train3_img_a.loc[i,"p_name"] = p_name
+        img_b = dict_cropped_img_train3[p_name]
+        score = bootstrap_model.predict([img_a,img_b])[0][0]
+        score_train3_img_a.loc[i,"score"] = score
+
+    score_img_a = pd.concat([score_test_img_a,
+                             score_train0_img_a,
+                             score_train1_img_a,
+                             score_train2_img_a,
+                             score_train3_img_a])
+
+    score_img_a.to_csv("/home/wencai/PycharmProjects/WhaleIP/Humpback-Whale-Identification-1st-/z_script/bootstrap_result/{}.csv".format(img_a_name))
+# score_img_a = pd.read_csv("/home/wencai/PycharmProjects/WhaleIP/img_a_score.csv")
+# print(score_img_a.sort_values(by=["score"],ascending=False).head(21))
 # for p_name in range(len(dict_img_all)):
 #     img = dict_img_all[p_name]
 #     score = standard_model.predict([img_a,img_b])[0][0]
