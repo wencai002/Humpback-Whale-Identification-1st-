c209c77459e5ab2a9b3d9a15cf25d711dc88cd36
wencai002 <cai.wenjie002@gmail.com>
2020-03-01 23:32:10 +0100

    20200301 ready for first submission

---
+++ z_script/submit_20200301.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df_result=pd.read_csv("/home/wencai/PycharmProjects/WhaleIP/Humpback-Whale-Identification-1st-/z_script/score_result/807.csv")
+df_result=df_result.drop(columns=["Unnamed: 0"])
+
+df_submit = pd.DataFrame()
+for r in range(df_result.shape[0]):
+    p_name = df_result.loc[r,"p_name"]
+    df_submit.loc[r, "p_name"] = p_name
+    df_result_r = df_result.loc[r,:]
+    df_result_r = df_result_r[1:]
+    ls_r_21 = df_result_r.sort_values(ascending=False)[0:21].index
+    ls_r_20 = []
+    for item in ls_r_21:
+        if item != p_name:
+            ls_r_20.append(item)
+        else: pass
+    for i in range(20):
+        df_submit.loc[r,i] = ls_r_20[i]
+
+df_submit.to_csv("/home/wencai/PycharmProjects/WhaleIP/Humpback-Whale-Identification-1st-/z_script/submit.csv")
